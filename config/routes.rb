2a16e87151cfba301394455876031f76bd507b8f
github username <github email address>
2020-02-24 21:35:39 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  #   /\ /\ /\ /\ /\
+  #there is a better way to do this using except \/ \/ \/ \/
+
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "party thyme",description: "ladidadidadi we like to party party")
+Article.create(title: "party 523thyme",description: "ladidadidadi346gh we like to party party")
+Article.create(title: "party fgthyme",description: "ladidadidadi we likedfsgsghto party party")
+Article.create(title: "party ljkhthyme",description: "ladidad534 tgvrhidadi we like to party party")
